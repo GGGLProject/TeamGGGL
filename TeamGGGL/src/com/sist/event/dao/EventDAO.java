07673b79ddfaf6bd7acc4c55389b8575484240ff
SIST_235 <SIST_235@SIST235>
2017-12-21 19:29:10 +0900

    이벤트 수정했습니다 확인요!

---
+++ TeamGGGL/src/com/sist/event/dao/EventDAO.java
@@ -0,0 +1,67 @@
+package com.sist.event.dao;
+
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+
+
+import java.io.*;
+import java.util.*;
+
+public class EventDAO {
+	private static SqlSessionFactory ssf;
+	static
+	{ // 이벤트DAO
+		try {
+			
+			Reader reader=Resources.getResourceAsReader("Config.xml");
+			ssf = new SqlSessionFactoryBuilder().build(reader);
+			//파싱 완료
+		}
+		catch(Exception ex)			
+			{
+			System.out.println(ex.getMessage());
+		}
+	}
+	public static List<EventVO> eventListData(Map map)
+	{
+		SqlSession session = ssf.openSession();
+		List<EventVO> list = session.selectList("eventListData",map);
+		session.close();
+		return list;
+	}
+	public static EventVO eventContentData(int no)
+	{
+		SqlSession session = ssf.openSession();
+		session.update("eventdHitIncrement", no);
+		session.commit();
+		EventVO vo = session.selectOne("eventContentData",no);
+		session.close();
+		return vo;
+	}
+	public static void eventInsert(EventVO vo)
+	{
+		SqlSession session= ssf.openSession(true);
+		session.insert("eventInsert",vo);
+		session.close();
+	}
+	// 총페이지 
+	   public static int eventTotalPage() {
+	      int total = 0;
+	      SqlSession session = null;
+	      
+	      try {
+	         // session : connection 연결
+	         session = ssf.openSession();
+	         total = session.selectOne("eventTotalPage");
+	      } catch(Exception e) {
+	         System.out.println(e.getMessage());
+	      } finally {
+	         // 반환
+	         if(session!=null)
+	            session.close();
+	      }
+	      return total;
+	   }
+}
---
+++ TeamGGGL/src/com/sist/event/dao/EventVO.java
@@ -0,0 +1,93 @@
+package com.sist.event.dao;
+
+import java.util.*;
+
+public class EventVO { 
+	//TABLE gggl_event!
+	//컬럼명과 일치되는 변수명 
+	// date 자료형은 java.util.date;
+	private int event_no;
+	private String event_title;
+	private String event_id;
+	private String event_content;
+	private Date event_day;
+	private Date evnet_regdate;
+	private String event_place;
+	private String event_category;
+	private String event_city;
+	private int event_hit;
+	private String event_image;
+//	EVENT_HIT               NUMBER        
+//	EVENT_IMAGE             VARCHAR2(500) 
+//	두개 추가
+
+	
+	public int getEvent_hit() {
+		return event_hit;
+	}
+	public void setEvent_hit(int event_hit) {
+		this.event_hit = event_hit;
+	}
+	public String getEvent_image() {
+		return event_image;
+	}
+	public void setEvent_image(String event_image) {
+		this.event_image = event_image;
+	}
+	public int getEvent_no() {
+		return event_no;
+	}
+	public void setEvent_no(int event_no) {
+		this.event_no = event_no;
+	}
+	public String getEvent_title() {
+		return event_title;
+	}
+	public void setEvent_title(String event_title) {
+		this.event_title = event_title;
+	}
+	public String getEvent_id() {
+		return event_id;
+	}
+	public void setEvent_id(String event_id) {
+		this.event_id = event_id;
+	}
+	public String getEvent_content() {
+		return event_content;
+	}
+	public void setEvent_content(String event_content) {
+		this.event_content = event_content;
+	}
+	public Date getEvent_day() {
+		return event_day;
+	}
+	public void setEvent_day(Date event_day) {
+		this.event_day = event_day;
+	}
+	public Date getEvnet_regdate() {
+		return evnet_regdate;
+	}
+	public void setEvnet_regdate(Date evnet_regdate) {
+		this.evnet_regdate = evnet_regdate;
+	}
+	public String getEvent_place() {
+		return event_place;
+	}
+	public void setEvent_place(String event_place) {
+		this.event_place = event_place;
+	}
+	public String getEvent_category() {
+		return event_category;
+	}
+	public void setEvent_category(String event_category) {
+		this.event_category = event_category;
+	}
+	public String getEvent_city() {
+		return event_city;
+	}
+	public void setEvent_city(String event_city) {
+		this.event_city = event_city;
+	}
+
+	
+}
