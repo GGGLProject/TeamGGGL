805eb2bf85914ee7123f992480f17e4edc91238a
user <user@user-pc>
2017-12-28 17:59:13 +0900

    개라리에 데이타넣기

--- TeamGGGL/src/com/sist/gallery/dao/GalleryDAO.java
+++ TeamGGGL/src/com/sist/gallery/dao/GalleryDAO.java
@@ -1,5 +1,125 @@
 package com.sist.gallery.dao;
 
+import java.io.Reader;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+
+import com.sist.gallery.dao.GalleryVO;
+
 public class GalleryDAO {
-	 //보라바보
+	private static SqlSessionFactory ssf;
+	static { 		// 내가 호출하지 않아도 자동으로 호출
+		try {
+			// XML 읽기
+			Reader reader = Resources.getResourceAsReader("Config.xml");
+			// XML 파싱
+			ssf = new SqlSessionFactoryBuilder().build(reader);
+		} catch (Exception e) {
+			System.out.println(e.getMessage());
+		}
+	}
+	
+	public static List<GalleryVO> galleryListAll(Map map) {
+		List<GalleryVO> list = new ArrayList<GalleryVO>();
+		SqlSession session = ssf.openSession();	// 주소값 얻어올 때 사용(getConnenction)
+		try {
+			list = session.selectList("galleryListAll", map);
+		} catch (Exception e) {
+			System.out.println("galleryListAll : " + e.getMessage());
+		} finally {
+			if(session!=null)
+				session.close(); // 반환
+		}
+		return list;
+	}
+	
+	public static List<GalleryVO> galleryListTop(Map map) {
+		List<GalleryVO> list = new ArrayList<GalleryVO>();
+		SqlSession session = ssf.openSession();	// 주소값 얻어올 때 사용(getConnenction)
+		try {
+			list = session.selectList("galleryListTop", map);
+		} catch (Exception e) {
+			System.out.println("galleryListTop : " + e.getMessage());
+		} finally {
+			if(session!=null)
+				session.close(); // 반환
+		}
+		return list;
+	}
+	
+	public static List<GalleryVO> galleryListMid(Map map) {
+		List<GalleryVO> list = new ArrayList<GalleryVO>();
+		SqlSession session = ssf.openSession();	// 주소값 얻어올 때 사용(getConnenction)
+		try {
+			list = session.selectList("galleryListMid", map);
+		} catch (Exception e) {
+			System.out.println("galleryListMid : " + e.getMessage());
+		} finally {
+			if(session!=null)
+				session.close(); // 반환
+		}
+		return list;
+	}
+	
+	public static List<GalleryVO> galleryListJungle(Map map) {
+		List<GalleryVO> list = new ArrayList<GalleryVO>();
+		SqlSession session = ssf.openSession();	// 주소값 얻어올 때 사용(getConnenction)
+		try {
+			list = session.selectList("galleryListJungle", map);
+		} catch (Exception e) {
+			System.out.println("galleryListJungle : " + e.getMessage());
+		} finally {
+			if(session!=null)
+				session.close(); // 반환
+		}
+		return list;
+	}
+	
+	public static List<GalleryVO> galleryListBottom(Map map) {
+		List<GalleryVO> list = new ArrayList<GalleryVO>();
+		SqlSession session = ssf.openSession();	// 주소값 얻어올 때 사용(getConnenction)
+		try {
+			list = session.selectList("galleryListBottom", map);
+		} catch (Exception e) {
+			System.out.println("galleryListBottom : " + e.getMessage());
+		} finally {
+			if(session!=null)
+				session.close(); // 반환
+		}
+		return list;
+	}
+	
+	public static List<GalleryVO> galleryListSupport(Map map) {
+		List<GalleryVO> list = new ArrayList<GalleryVO>();
+		SqlSession session = ssf.openSession();	// 주소값 얻어올 때 사용(getConnenction)
+		try {
+			list = session.selectList("galleryListSupport", map);
+		} catch (Exception e) {
+			System.out.println("galleryListSupport : " + e.getMessage());
+		} finally {
+			if(session!=null)
+				session.close(); // 반환
+		}
+		return list;
+	}
+	
+	public static int galleryTotalPage() {
+		int total = 0;
+		SqlSession session = ssf.openSession();
+		try {
+			total = session.selectOne("galleryTotalPage");
+		} catch (Exception e) {
+			System.out.println("galleryTotalPage : " + e.getMessage());
+		} finally {
+			if(session!=null)
+				session.close(); // 반환
+		}
+		return total;
+	}
 }
--- TeamGGGL/src/com/sist/model/GalleryModel.java
+++ TeamGGGL/src/com/sist/model/GalleryModel.java
@@ -1,17 +1,48 @@
 package com.sist.model;
 
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import com.sist.controller.Controller;
 import com.sist.controller.RequestMapping;
+import com.sist.gallery.dao.*;
 
 @Controller
 public class GalleryModel {
    @RequestMapping("gallery.do")
    public String gallery(HttpServletRequest req,HttpServletResponse res)
    {
-	   req.setAttribute("main_jsp", "../gameGallery/gallery.jsp");
-	   return "gameMain/main.jsp";
+	    String page = req.getParameter("page");
+		if(page==null)
+			page="1";
+		int curpage = Integer.parseInt(page);
+		int rowSize = 12;
+		int start = (rowSize*curpage)-(rowSize-1);
+		int end = rowSize*curpage;
+		Map map = new HashMap();
+		map.put("start", start);	// movie-mapper.xml이 받는다.
+		map.put("end", end);
+		List<GalleryVO> list_all = GalleryDAO.galleryListAll(map);
+		List<GalleryVO> list_t = GalleryDAO.galleryListTop(map);
+		List<GalleryVO> list_m = GalleryDAO.galleryListMid(map);
+		List<GalleryVO> list_j = GalleryDAO.galleryListJungle(map);
+		List<GalleryVO> list_b = GalleryDAO.galleryListBottom(map);
+		List<GalleryVO> list_s = GalleryDAO.galleryListSupport(map);
+		int totalpage = GalleryDAO.galleryTotalPage();
+	   
+		req.setAttribute("curpage", curpage);
+		req.setAttribute("totalpage", totalpage);
+		req.setAttribute("list_all", list_all);
+		req.setAttribute("list_t", list_t);
+		req.setAttribute("list_m", list_m);
+		req.setAttribute("list_j", list_j);
+		req.setAttribute("list_b", list_b);
+		req.setAttribute("list_s", list_s);
+	    req.setAttribute("main_jsp", "../gameGallery/gallery.jsp");
+	    return "gameMain/main.jsp";
    }
 }
