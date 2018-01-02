bd0fa0921205b730d052d69f37975adc04c59c43
오지널 <hyeok7777@gmail.com>
2018-01-02 20:00:33 +0900

    뉴스 댓글 추가 가능

--- TeamGGGL/src/com/sist/model/NewsModel.java
+++ TeamGGGL/src/com/sist/model/NewsModel.java
@@ -7,69 +7,111 @@ import java.util.Map;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
 
 import com.sist.controller.Controller;
 import com.sist.controller.RequestMapping;
 import com.sist.news.dao.NewsDAO;
+import com.sist.news.dao.NewsReplyVO;
 import com.sist.news.dao.NewsVO;
 
 @Controller
 public class NewsModel {
+	@RequestMapping("news_reply_new_insert.do")
+	public String news_reply_new_insert(HttpServletRequest req, HttpServletResponse res)
+	{
+		try {
+			req.setCharacterEncoding("EUC-KR");
+		} catch (Exception ex) {
+		}
+		HttpSession session = req.getSession();
+		String bno = req.getParameter("bno");
+		String msg = req.getParameter("msg");
+		String id = (String) session.getAttribute("email");
+		String name = (String) session.getAttribute("name");
+		NewsReplyVO rvo = new NewsReplyVO();
+		rvo.setBno(Integer.parseInt(bno));
+		rvo.setMsg(msg);
+		rvo.setId(id);
+		rvo.setName(name);
+		NewsDAO.replyNewInsert(rvo);
+		req.setAttribute("no", bno);
+		return "gameNews/reply_ok.jsp";
+	}
+	
 	@RequestMapping("news_detail.do")
 	public String news_detail(HttpServletRequest req, HttpServletResponse res) {
-		req.setAttribute("main_jsp", "../gameNews/news_detail.jsp");
-		String no=req.getParameter("no");
-		
+		String no = req.getParameter("no");
+		// ${sessionScope.name }
+		List<NewsReplyVO> replylist = NewsDAO.replyListData(Integer.parseInt(no));
 		NewsVO vo = NewsDAO.newsDetailData(Integer.parseInt(no));
+		req.setAttribute("replylist", replylist);
 		req.setAttribute("vo", vo);
-		//NewsVO vo = NewsDAO.newsDetailData(Integer.parseInt(arg0))
 		req.setAttribute("main_jsp", "../gameNews/news_detail.jsp");
-		return "gameMain/main.jsp";	}
-	
+		return "gameMain/main.jsp";
+	}
+
 	@RequestMapping("news_main.do")
-	public String news_main(HttpServletRequest req, HttpServletResponse res) throws UnsupportedEncodingException{
+	public String news_main(HttpServletRequest req, HttpServletResponse res) throws UnsupportedEncodingException {
 		req.setCharacterEncoding("EUC-KR");
 		
-		String page=req.getParameter("page");
-		
-		if(page == null)
-			page="1";
-		int curpage=Integer.parseInt(page);
+		String page = req.getParameter("page");
+		String no = req.getParameter("no");
+		if (page == null)
+			page = "1";
+		int curpage = Integer.parseInt(page);
 		String category = req.getParameter("category");
-		if(category==null)
-			category="전체";
-		int rowSize=10;
-		int start = (rowSize*curpage)-(rowSize-1);
-		int end = rowSize*curpage;
+		if (category == null)
+			category = "전체";
+		int rowSize = 10;
+		int start = (rowSize * curpage) - (rowSize - 1);
+		int end = rowSize * curpage;
 		Map map = new HashMap();
 		map.put("start", start);
-		map.put("end", end);	
-		if(category.equals("전체"))
-		{
-		   map.put("category", "all");	
-		}
-		else
-		{
-		   map.put("category", category);
+		map.put("end", end);
+		if (category.equals("전체")) {
+			map.put("category", "all");
+		} else {
+			map.put("category", category);
 		}
 		System.out.println(category);
-		
-
 		
 		List<NewsVO> list = NewsDAO.newsListData(map);
 		List<NewsVO> ulist = NewsDAO.newsUpdateListData();
 		int newsTotalCount = NewsDAO.newsTotalCount();
 		int totalpage = NewsDAO.newsTotalPage();
 		List<NewsVO> rlist = NewsDAO.newsRecommandData();
-		req.setAttribute("rlist", rlist);
 		
+		req.setAttribute("rlist", rlist);
 		req.setAttribute("list", list);
 		req.setAttribute("ulist", ulist);
-		req.setAttribute("curpage", curpage);		
+		req.setAttribute("curpage", curpage);
 		req.setAttribute("totalpage", totalpage);
 		req.setAttribute("newsTotalCount", newsTotalCount);
 		req.setAttribute("main_jsp", "../gameNews/news_main.jsp");
 		return "gameMain/main.jsp";
 	}
-	
+
+//	@RequestMapping("news_reply_new_insert.do")
+//	public String reply_new_insert(HttpServletRequest req, HttpServletResponse res) {
+//		try {
+//			req.setCharacterEncoding("EUC-KR");
+//		} catch (Exception ex) {
+//		}
+//		HttpSession session = req.getSession();
+//		String bno = req.getParameter("bno");
+//		String msg = req.getParameter("msg");
+//		String id = (String) session.getAttribute("id");
+//		String name = (String) session.getAttribute("name");
+//		NewsReplyVO vo = new NewsReplyVO();
+//		vo.setBno(Integer.parseInt(bno));
+//		vo.setMsg(msg);
+//		vo.setId(id);
+//		vo.setName(name);
+//		NewsDAO.replyNewInsert(vo);
+//		req.setAttribute("no", bno);
+//		return "gameNews/news_detail.jsp";
+//	}
+
 }
